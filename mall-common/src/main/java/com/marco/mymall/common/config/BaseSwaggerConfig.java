de63dcd5821183f76a3caceffa05e5fdaf19f9f3
xiaoliutong <741990882@qq.com>
2020-09-23 17:36:15 +0800

    2020-09-23 admin

--- mall-common/src/main/java/com/marco/mymall/common/config/BaseSwaggerConfig.java
+++ mall-common/src/main/java/com/marco/mymall/common/config/BaseSwaggerConfig.java
@@ -5,21 +5,23 @@ import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 刘壮壮
  * 这个是swagger基础配置类，每个模块可以通过继承这个配置类 实现swaggerProperties
  * 方法来实现swagger的配置
- *
  */
 public abstract class BaseSwaggerConfig {
     /**
-     * @apiNote 这里设置为抽象方法是为了让实现类，必须完成一些自定义的配置
      * @return
+     * @apiNote 这里设置为抽象方法是为了让实现类，必须完成一些自定义的配置
      */
     abstract SwaggerProperties swaggerProperties();
 
@@ -35,20 +37,51 @@ public abstract class BaseSwaggerConfig {
                 .paths(PathSelectors.any())
                 .build();
         //通过判断是否是安全框架来进行相应的配置
-        /*if (swaggerProperties.isEnableSecurity()) {
+        if (swaggerProperties.isEnableSecurity()) {
             docket.securitySchemes(securitySchemes()).securityContexts(securityContexts());
-        }*/
+        }
         return docket;
     }
+    private List<ApiKey> securitySchemes() {
+        //设置请求头信息
+        List<ApiKey> result = new ArrayList<>();
+        ApiKey apiKey = new ApiKey("Authorization", "Authorization", "header");
+        result.add(apiKey);
+        return result;
+    }
 
-
-    private ApiInfo apiInfo(SwaggerProperties swaggerProperties){
+    private ApiInfo apiInfo(SwaggerProperties swaggerProperties) {
         return new ApiInfoBuilder().title(swaggerProperties.getTitle()).
                 version(swaggerProperties.getVersion()).
                 description(swaggerProperties.getDescription()).
                 contact(new Contact(swaggerProperties.getContactName(),
-                        swaggerProperties.getContactUrl(),swaggerProperties.getContactEmail()))
+                        swaggerProperties.getContactUrl(), swaggerProperties.getContactEmail()))
                 .build();
 
     }
+
+
+    private List<SecurityContext> securityContexts() {
+        //设置需要登录认证的路径
+        List<SecurityContext> result = new ArrayList<>();
+        result.add(getContextByPath("/*/.*"));
+        return result;
+    }
+
+    private SecurityContext getContextByPath(String pathRegex) {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(pathRegex))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        List<SecurityReference> result = new ArrayList<>();
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        result.add(new SecurityReference("Authorization", authorizationScopes));
+        return result;
+    }
+
 }
